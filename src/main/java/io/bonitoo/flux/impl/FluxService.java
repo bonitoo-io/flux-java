32354c3f113cd997975b111b07a7fad727c6e891
Jakub Bednar <jakub.bednar@gmail.com>
2018-08-21 14:01:35 +0200

    Fixed new response format without default values and types

--- src/main/java/io/bonitoo/flux/impl/FluxService.java
+++ src/main/java/io/bonitoo/flux/impl/FluxService.java
@@ -24,9 +24,12 @@ package io.bonitoo.flux.impl;
 
 import javax.annotation.Nonnull;
 
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import retrofit2.http.Streaming;
@@ -40,8 +43,9 @@ public interface FluxService {
     @Streaming
     @POST("/query")
     @Nonnull
-    Call<ResponseBody> query(@Query(value = "query", encoded = true) String query,
-                             @Query(value = "organization", encoded = true) String orgID);
+    @Headers("Content-Type: application/json")
+    Call<ResponseBody> query(@Nonnull @Query(value = "organization", encoded = true) String orgID,
+                             @Nonnull @Body RequestBody batchPoints);
 
     @GET("/ping")
     Call<ResponseBody> ping();
